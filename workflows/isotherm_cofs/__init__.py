8318043b2727b1f74e3cf25ce8effc9579d14015
Daniele Ongari <daniele.ongari@gmail.com>
2018-10-29 19:21:41 +0100

    1) cleaned some stuff 2) Resetted workflows/isotherm that was tested and correcly working:     this will be now kept for testing the merges from sash 3) Created workflow/isotherm_cofs to be adapted rto my applications

--- workflows/isotherm_cofs/__init__.py
+++ workflows/isotherm_cofs/__init__.py
@@ -46,6 +46,9 @@ RemoteData = DataFactory('remote')
 StructureData = DataFactory('structure')
 
 def multiply_unit_cell (cif, threshold):
+    '''
+
+    '''
     from math import cos, sin, sqrt, pi
     import numpy as np
     deg2rad=pi/180.
@@ -63,8 +66,9 @@ def multiply_unit_cell (cif, threshold):
 
     # this code makes sure that the structure is replicated enough times in x, y, z direction
     # in order to be compatible with the threshold value
-    
+
     # first step is computing cell parameters according to  https://en.wikipedia.org/wiki/Fractional_coordinates
+    # Note: this is the algorithm implemented in Raspa (framework.c/UnitCellBox). There also is a simpler one but it is less robust.
     v = sqrt(1-cos(alpha)**2-cos(beta)**2-cos(gamma)**2+2*cos(alpha)*cos(beta)*cos(gamma))
     cell=np.zeros((3,3))
     cell[0,:] = [a, 0, 0]
@@ -95,17 +99,17 @@ spin = {
         "Cl" : 0.0,
         "K"  : 0.0,
         "Ca" : 0.0,
-        "Sc" : 1.0 / 2.0,
-        "Ti" : 2.0 / 2.0,
-        "V"  : 3.0 / 2.0,
-        "Cr" : 4.0 / 2.0,
-        "Mn" : 5.0 / 2.0,
-        "Fe" : 4.0 / 2.0,
-        "Co" : 3.0 / 2.0,
-        "Ni" : 2.0 / 2.0,
-        "Cu" : 1.0 / 2.0,
-        "Zn" : 0.0,
-        "Zr" : 2.0 / 2.0,
+        "Sc" : 1.0 / 2.0,  # oxidation state +2, high spin
+        "Ti" : 2.0 / 2.0,  # oxidation state +2, high spin
+        "V"  : 3.0 / 2.0,  # oxidation state +2, high spin
+        "Cr" : 4.0 / 2.0,  # oxidation state +2, high spin
+        "Mn" : 5.0 / 2.0,  # oxidation state +2, high spin
+        "Fe" : 4.0 / 2.0,  # oxidation state +2, high spin
+        "Co" : 3.0 / 2.0,  # oxidation state +2, high spin
+        "Ni" : 2.0 / 2.0,  # oxidation state +2, high spin
+        "Cu" : 1.0 / 2.0,  # oxidation state +2, high spin
+        "Zn" : 0.0,        # oxidation state +2, high spin
+        "Zr" : 2.0 / 2.0,  # oxidation state +2, high spin
         }
 
 @wf
@@ -134,7 +138,7 @@ class Isotherm(WorkChain):
     @classmethod
     def define(cls, spec):
         super(Isotherm, cls).define(spec)
-        
+
         # structure, adsorbant, pressures
         spec.input('structure', valid_type=CifData)
         spec.input("probe_molecule", valid_type=ParameterData)
@@ -209,7 +213,7 @@ class Isotherm(WorkChain):
         # Trying to guess the multiplicity of the system
         inputs = {
             'code'      : self.inputs.cp2k_code,
-            'structure' : new_structure, 
+            'structure' : new_structure,
             '_options'  : self.inputs._cp2k_options,
             '_label'    : "Cp2kRobustGeoOptWorkChain",
         }
@@ -219,29 +223,26 @@ class Isotherm(WorkChain):
             self.ctx.cp2k_parameters = guess_multiplicity(new_structure)
             inputs['parameters'] = self.ctx.cp2k_parameters
 
-        # uncomment for the test runs
-        # FROM HERE
-        #params_dict = ParameterData(dict={
-        #        'MOTION':{
-        #            'MD':{
-        #                'STEPS': 5,
-        #                },
-        #            'GEO_OPT': {
-        #                'MAX_ITER': 5,
-        #            },
-        #            'CELL_OPT': {
-        #                'MAX_ITER': 5,
-        #            },
-        #        },
-        #        }).store()
-        #inputs['parameters'] = merge_ParameterData(self.ctx.cp2k_parameters, params_dict)
-        # TILL HERE
+        params_dict = ParameterData(dict={
+                'MOTION':{
+                    'MD':{
+                        'STEPS': 50,      #NVT @ 300K
+                        },
+                    'GEO_OPT': {
+                        'MAX_ITER': 50,   #BFGS, default CP2K's tresholds
+                    },
+                    'CELL_OPT': {
+                        'MAX_ITER': 100,  #BFGS w/FIX_ANGLES, larger tresholds
+                    },
+                },
+                }).store()
+        inputs['parameters'] = merge_ParameterData(self.ctx.cp2k_parameters, params_dict)
 
         # Create the calculation process and launch it
         running = submit(Cp2kRobustGeoOptWorkChain, **inputs)
         self.report("pk: {} | Running Cp2kRobustGeoOptWorkChain to optimize geometry".format(running.pid))
         return ToContext(geo_opt_calc=Outputs(running))
-    
+
     def parse_geo_opt(self):
         """Extract optimized structure and put it into self.ctx.structure"""
         self.ctx.structure = self.ctx.geo_opt_calc['output_structure']
@@ -258,8 +259,9 @@ class Isotherm(WorkChain):
             'structure'          : self.ctx.structure,
             'cp2k_code'          : self.inputs.cp2k_code,
             'cp2k_parameters'    : self.ctx.cp2k_parameters,
-            'cp2k_parent_folder' : self.ctx.geo_opt_calc['remote_folder'],
             '_cp2k_options'      : self.inputs._cp2k_options,
+            'cp2k_parent_folder' : self.ctx.geo_opt_calc['remote_folder'],
+            #'ddec_parameters'
             'ddec_code'          : self.inputs.ddec_code,
             '_ddec_options'      : self.inputs._ddec_options,
             '_label'             : "DdecCp2kChargesWorkChain",
@@ -336,7 +338,7 @@ class Isotherm(WorkChain):
         """Extract the pressure and loading average of the last completed raspa calculation"""
         self.ctx.restart_raspa_calc = self.ctx.raspa_loading['retrieved_parent_folder']
         pressure = self.ctx.raspa_parameters['GeneralSettings']['ExternalPressure']/1e5
-        loading_average = self.ctx.raspa_loading["component_0"].dict.loading_absolute_average
+        loading_average = self.ctx.raspa_loading["component_0"].dict.loading_absolute_average 
         self.ctx.result.append((pressure, loading_average))
 
     def run_henry_raspa(self):
--- workflows/isotherm_cofs/run_isotherm_wf.py
+++ workflows/isotherm_cofs/run_isotherm_wf.py
@@ -8,12 +8,17 @@ from aiida.work.run import submit
 from ase.io import read
 from workflows.isotherm import Isotherm
 
+from glob import glob
+
 # data objects
 ArrayData = DataFactory('array')
 ParameterData = DataFactory('parameter')
 CifData = DataFactory('cif')
 
-structure = CifData(file=os.getcwd()+'/Cu-MOF-74.cif')
+cp2k_code = test_and_get_code('cp2k-5.1@fidis', expected_code_type='cp2k')
+ddec_code = test_and_get_code('ddec@fidis', expected_code_type='ddec')
+zeopp_code = test_and_get_code('zeopp@deneb', expected_code_type='zeopp.network')
+raspa_code = test_and_get_code('raspa@deneb', expected_code_type='raspa')
 
 cp2k_options = {
     "resources": {
@@ -26,17 +31,16 @@ ddec_options = {
     "resources": {
         "num_machines": 1,
     },
-    "max_wallclock_seconds": 1 * 60 * 60 / 2,
+    "max_wallclock_seconds": 1 * 60 * 60,
     "withmpi": False,
     }
 
-# option for zeo++ and raspa
 zr_options = {
     "resources": {
         "num_machines": 1,
         "tot_num_mpiprocs": 1,
     },
-    "max_wallclock_seconds": 1 * 60 * 60,
+    "max_wallclock_seconds": 72 * 60 * 60,
     "withmpi": False,
     }
 
@@ -44,13 +48,10 @@ raspa_parameters = ParameterData(dict={
         "GeneralSettings":
         {
         "SimulationType"                   : "MonteCarlo",
-        #"NumberOfCycles"                   : 100000,
-        "NumberOfCycles"                   : 1000,
-        #"NumberOfInitializationCycles"     : 10000,   # 20000
-        "NumberOfInitializationCycles"     : 2000,   # 20000
+        "NumberOfCycles"                   : 100000,
+        "NumberOfInitializationCycles"     : 10000,  
 
-        #"PrintEvery"                       : 10000,
-        "PrintEvery"                       : 1000,
+        "PrintEvery"                       : 100,
 
         "ChargeMethod"                     : "Ewald",
         "CutOff"                           : 12.0,
@@ -58,7 +59,7 @@ raspa_parameters = ParameterData(dict={
         "EwaldPrecision"                   : 1e-6,
 
         "Framework"                        : 0,
-        "UnitCells"                        : "1 1 1", #TODO: expand to be 2*cutoff
+        "UnitCells"                        : "1 1 1",  #needs to be expanded!
         "HeliumVoidFraction"               : 0.0,
 
         "ExternalTemperature"              : 298.0,
@@ -76,19 +77,20 @@ raspa_parameters = ParameterData(dict={
         }],
         })
 
-cp2k_code = test_and_get_code('cp2k-5.1@fidis-debug', expected_code_type='cp2k')
-ddec_code = test_and_get_code('ddec@fidis-debug', expected_code_type='ddec')
-zeopp_code = test_and_get_code('zeopp@deneb', expected_code_type='zeopp.network')
-raspa_code = test_and_get_code('raspa@deneb', expected_code_type='raspa')
-
-
 pressures = ArrayData()
 pressures.set_array("pressures", np.array([0.01e5, 0.05e5, 0.1e5, 0.15e5, 0.2e5]))
+
+all_structures = glob(path.abspath("/home/daniele/Programs/aiida-database/frameworks/corecofs/13150N.cif"))
+for s in all_structures:
+    structure = CifData(file=s)
+    structure.label = s.split('/')[-1]
+    structure.store()
+
 submit(Isotherm,
         structure=structure,
         probe_molecule=ParameterData(dict={"sigma":1.525}),
         pressures=pressures,
-        min_cell_size=Float(10.0),
+        min_cell_size=Float(10.0), 
         cp2k_code=cp2k_code,
         _cp2k_options=cp2k_options,
         ddec_code=ddec_code,
